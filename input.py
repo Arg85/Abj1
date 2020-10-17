b85e1a6839d1df010c10bdb60d07c0c8a2852022
Arg85 <48096235+Arg85@users.noreply.github.com>
2020-10-17 20:09:09 +0530

    Update input.py

--- input.py
+++ input.py
@@ -2,3 +2,8 @@ a=input()
 print(a)
 b=int(input("no only"))
 print(b)
+# or if for n array we can use map
+a=list(map(int,input().strip().split())
+# same can be done with loop if we know size of array
+for i in range(6):
+       a[i]=input()
